911fb92020159872594c986bd3fa5447fd1cccc3
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-05-07 22:17:19 +0530

    feat: implement 3-tier Goals system and fix Android networking permissions
    - Created GoalModel with support for Weekly, Monthly, and Career types
    - Implemented GoalRepository and Supabase integration for persistent storage
    - Built GoalsScreen with TabBar navigation and neon-themed GoalCard widgets
    - Added AddGoalBottomSheet for interactive goal creation
    - Fixed SocketException by adding INTERNET and ACCESS_NETWORK_STATE permissions to AndroidManifest.xml
    - Generated Day 17 technical documentation for interview preparation

---
+++ lib/features/goals/data/repositories/goal_repository_impl.dart
@@ -0,0 +1,65 @@
+import 'package:injectable/injectable.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../domain/models/goal_model.dart';
+import '../../domain/repositories/goal_repository.dart';
+
+@LazySingleton(as: GoalRepository)
+class GoalRepositoryImpl implements GoalRepository {
+  final SupabaseClient _supabase;
+
+  GoalRepositoryImpl(this._supabase);
+
+  @override
+  Future<List<GoalModel>> getGoals(String userId, {GoalType? type}) async {
+    var query = _supabase.from('goals').select().eq('user_id', userId);
+    
+    if (type != null) {
+      query = query.eq('type', type.name);
+    }
+    
+    final response = await query.order('created_at', ascending: false);
+    
+    return (response as List).map((json) => GoalModel.fromJson(json)).toList();
+  }
+
+  @override
+  Future<GoalModel> createGoal(GoalModel goal) async {
+    final data = goal.toJson();
+    if (goal.id.isEmpty) {
+      data.remove('id');
+    }
+
+    final response = await _supabase
+        .from('goals')
+        .insert(data)
+        .select()
+        .single();
+    
+    return GoalModel.fromJson(response);
+  }
+
+  @override
+  Future<GoalModel> updateGoal(GoalModel goal) async {
+    final response = await _supabase
+        .from('goals')
+        .update(goal.toJson())
+        .eq('id', goal.id)
+        .select()
+        .single();
+    
+    return GoalModel.fromJson(response);
+  }
+
+  @override
+  Future<void> deleteGoal(String goalId) async {
+    await _supabase.from('goals').delete().eq('id', goalId);
+  }
+
+  @override
+  Future<void> updateGoalProgress(String goalId, int newValue) async {
+    await _supabase
+        .from('goals')
+        .update({'current_value': newValue})
+        .eq('id', goalId);
+  }
+}
---
+++ lib/features/goals/domain/repositories/goal_repository.dart
@@ -0,0 +1,9 @@
+import '../models/goal_model.dart';
+
+abstract class GoalRepository {
+  Future<List<GoalModel>> getGoals(String userId, {GoalType? type});
+  Future<GoalModel> createGoal(GoalModel goal);
+  Future<GoalModel> updateGoal(GoalModel goal);
+  Future<void> deleteGoal(String goalId);
+  Future<void> updateGoalProgress(String goalId, int newValue);
+}
